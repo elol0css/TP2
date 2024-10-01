cea3bdda39fa6aa18d28aa3ed9012d610e1a7686
elol0css <elodynadeau@gmail.com>
2024-10-01 18:16:03 -0400

    Add files via upload

---
+++ TP2/script/index.js
@@ -0,0 +1,78 @@
+// EXERCICE 1
+let phrase5MotTxt= prompt("Écrivez une phrase avec 5 mots maximum");
+let phrase5Mot= phrase5MotTxt.split(" ");
+let numMots= phrase5Mot.length;
+alert("Le nombre de mots dans la phrase " + numMots)
+
+let longueurMots= [];
+
+if (numMots == 0) {
+    console.log((longueurMots[0].length)=0 )
+    console.log((longueurMots[1].length)=0 )
+    console.log((longueurMots[2].length)=0 )
+    console.log((longueurMots[3].length)=0 )
+    console.log((longueurMots[4].length)=0 )
+    }
+
+
+longueurMots.push(phrase5Mot[0].length);
+
+
+if (numMots == 1) {
+    console.log((longueurMots[1].length)=0 )
+    console.log((longueurMots[2].length)=0 )
+    console.log((longueurMots[3].length)=0 )
+    console.log((longueurMots[4].length)=0 )
+    }
+
+
+longueurMots.push(phrase5Mot[1].length);
+
+
+if(numMots == 2) {
+    console.log((longueurMots[1].length)=0 )
+    console.log((longueurMots[2].length)=0 )
+    console.log((longueurMots[3].length)=0 )
+    console.log((longueurMots[4].length)=0 )
+    }   
+
+
+longueurMots.push(phrase5Mot[2].length);
+longueurMots.push(phrase5Mot[3].length);
+longueurMots.push(phrase5Mot[4].length);
+
+// si ya pas plus que x mot, alors les autres mots ont 0 lettres
+if (numMots < 1) {
+console.log((longueurMots[0].length)=0 )
+console.log((longueurMots[1].length)=0 )
+console.log((longueurMots[2].length)=0 )
+console.log((longueurMots[3].length)=0 )
+console.log((longueurMots[4].length)=0 )
+}
+else
+if(numMots < 2) {
+console.log((longueurMots[1].length)=0 )
+console.log((longueurMots[2].length)=0 )
+console.log((longueurMots[3].length)=0 )
+console.log((longueurMots[4].length)=0 )
+}
+
+
+
+
+
+
+
+
+
+
+// let motLong=
+// let motCourt=
+
+// EXERCICE 2
+
+// EXERCICE 3 
+
+// EXERCICE 4
+
+// EXERCICE 5
