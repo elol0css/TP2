805b6fcefab6a051947b2c796504e51ade62d121
elol0css <elodynadeau@gmail.com>
2024-10-04 20:21:03 -0400

    exercice 1 complet tester

--- TP2/script/index.js
+++ TP2/script/index.js
@@ -1,70 +1,87 @@
 // EXERCICE 1
 let phrase5MotTxt= prompt("Écrivez une phrase avec 5 mots maximum");
-let phrase5Mot= phrase5MotTxt.split(" ");
-let numMots= phrase5Mot.length;
-alert("Le nombre de mots dans la phrase " + numMots)
 
 let longueurMots= [];
-
-if (numMots == 0) {
-    console.log((longueurMots[0].length)=0 )
-    console.log((longueurMots[1].length)=0 )
-    console.log((longueurMots[2].length)=0 )
-    console.log((longueurMots[3].length)=0 )
-    console.log((longueurMots[4].length)=0 )
-    }
-
-
-longueurMots.push(phrase5Mot[0].length);
-
-
-if (numMots == 1) {
-    console.log((longueurMots[1].length)=0 )
-    console.log((longueurMots[2].length)=0 )
-    console.log((longueurMots[3].length)=0 )
-    console.log((longueurMots[4].length)=0 )
-    }
-
-
-longueurMots.push(phrase5Mot[1].length);
-
-
-if(numMots == 2) {
-    console.log((longueurMots[1].length)=0 )
-    console.log((longueurMots[2].length)=0 )
-    console.log((longueurMots[3].length)=0 )
-    console.log((longueurMots[4].length)=0 )
-    }   
-
-
-longueurMots.push(phrase5Mot[2].length);
-longueurMots.push(phrase5Mot[3].length);
-longueurMots.push(phrase5Mot[4].length);
-
-// si ya pas plus que x mot, alors les autres mots ont 0 lettres
-if (numMots < 1) {
-console.log((longueurMots[0].length)=0 )
-console.log((longueurMots[1].length)=0 )
-console.log((longueurMots[2].length)=0 )
-console.log((longueurMots[3].length)=0 )
-console.log((longueurMots[4].length)=0 )
+if(phrase5MotTxt.length == 0)
+{
+    alert("Le nombre de mots dans la phrase " + 0);
+    longueurMots.push("".length);
+    longueurMots.push("".length);
+    longueurMots.push("".length);
+    longueurMots.push("".length);
+    longueurMots.push("".length);
 }
-else
-if(numMots < 2) {
-console.log((longueurMots[1].length)=0 )
-console.log((longueurMots[2].length)=0 )
-console.log((longueurMots[3].length)=0 )
-console.log((longueurMots[4].length)=0 )
+else if(phrase5MotTxt.length >=1)
+{    
+    let phrase5Mot= phrase5MotTxt.split(" ");
+    let numMots= phrase5Mot.length;
+
+    alert("Le nombre de mots dans la phrase " + numMots);
+    let texte = " ";
+
+    if (numMots == 1) //toDo//remplacer par une boucle for
+        {
+            longueurMots.push(phrase5Mot[0].length);
+            longueurMots.push("".length);
+            longueurMots.push("".length);
+            longueurMots.push("".length);
+            longueurMots.push("".length);
+       }
+        else 
+        if(numMots == 2)
+            {
+            longueurMots.push(phrase5Mot[0].length);
+            longueurMots.push(phrase5Mot[1].length);
+            longueurMots.push("".length);
+            longueurMots.push("".length);
+            longueurMots.push("".length);
+            } 
+
+            else 
+            if (numMots == 3)
+                {
+                    longueurMots.push(phrase5Mot[0].length);
+                    longueurMots.push(phrase5Mot[1].length);
+                    longueurMots.push(phrase5Mot[2].length);
+                    longueurMots.push("".length);
+                    longueurMots.push("".length);
+                    // texte = phrase5Mot[0].length + ", " + phrase5Mot[1].length + phrase5Mot[2].length +  " , 0, 0";            
+                    // alert("Les longueurs des mots respectivement : " + texte);
+                }
+
+            else if (numMots == 4)
+                {
+                    
+                    longueurMots.push(phrase5Mot[0].length);
+                    longueurMots.push(phrase5Mot[1].length);
+                    longueurMots.push(phrase5Mot[2].length);
+                    longueurMots.push(phrase5Mot[3].length);
+                    longueurMots.push("".length);
+                    //texte = phrase5Mot[0].length + ", " + phrase5Mot[1].length + ", " +  phrase5Mot[2].length + ", " + phrase5Mot[3].length + " , 0";            
+                    //alert("Les longueurs des mots respectivement : " + texte);
+                }
+                else if (numMots == 5)
+                    {
+                        
+                        longueurMots.push(phrase5Mot[0].length);
+                        longueurMots.push(phrase5Mot[1].length);
+                        longueurMots.push(phrase5Mot[2].length);
+                        longueurMots.push(phrase5Mot[3].length);
+                        longueurMots.push(phrase5Mot[4].length);
+                        //texte = phrase5Mot[0].length + ", " + phrase5Mot[1].length + ", " +  phrase5Mot[2].length + ", " + phrase5Mot[3].length + ", " + phrase5Mot[4].length;            
+                        //alert("Les longueurs des mots respectivement : " + texte);
+                    }
+
 }
+                          
+alert("Les longueurs des mots respectivement : " + longueurMots);
 
-
-
-
-
-
-
-
-
+// let longueurMots= [];
+// longueurMots.push(phrase5Mot[0].length);
+// longueurMots.push(phrase5Mot[1].length);
+// longueurMots.push(phrase5Mot[2].length);
+// longueurMots.push(phrase5Mot[3].length);
+// longueurMots.push(phrase5Mot[4].length);
 
 // let motLong=
 // let motCourt=
